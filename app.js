99b5d1e7c7ff7deff7484f706aa12eb98d424a79
arpansadhu10 <arpansadhu03@gmail.com>
2020-11-26 01:34:51 +0530

    buggy line problem

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const bodyParser=require('body-parser');
 const ejs=require('ejs');
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
+
 
 var items=[];
 var item;
